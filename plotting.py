5678e40e0c55ab97f064e38889c8e49d3b663a69
Jacob Rafati <yrafati@gmail.com>
2018-10-24 14:34:16 -0700

    staging for push/pull

--- plotting.py
+++ plotting.py
@@ -7,7 +7,7 @@ tasks = ['BeamRider-v0',
 		 'Seaquest-v0',
 		 'SpaceInvaders-v0']
 
-batch_list = [4096]
+batch_list = [512,1024,2048,4096]
 memory_list = [20,40,80]
 
 for m in memory_list: 
@@ -21,8 +21,12 @@ for m in memory_list:
 				print('max score for ',task,' = ',max(max(A[1]),max(A[4])))
 
 
-import pickle
-file_name = 'task_Breakout-v0_search_line-search_matrix_L-BFGS_memory_40_batch_512.pkl'
-with open(file_name, 'rb') as f:
-	A = pickle.load(f)
-	print('max score for  = ',max(max(A[1]),max(A[4])))
+for task in tasks:
+	max_score = -float('Inf')
+	for m in memory_list: 
+		for b in batch_list:
+			file_name = 'task_' + task + '_search_line-search_matrix_L-BFGS_memory_' + str(m) + '_batch_' + str(b) + '.pkl'
+			with open(file_name, 'rb') as f:
+				A = pickle.load(f)
+				max_score = max(max_score, max(max(A[1]),max(A[4])))
+	print('max score for ',task,' = ',max_score)
