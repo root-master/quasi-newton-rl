8958c43d5ec6e04d38569f3d4780eda693ca08f9
Jacob Rafati <yrafati@gmail.com>
2018-10-30 14:39:09 -0700

    new job config

--- plotting.py
+++ plotting.py
@@ -9,16 +9,18 @@ tasks = ['BeamRider-v0',
 
 batch_list = [512,1024,2048,4096]
 memory_list = [20,40,80]
-
+results_folder = './results/'
 for m in memory_list: 
 	for b in batch_list:
 		print('L-BFGS memory = ', m)
 		print('batch size = ', b)
 		for task in tasks:
 			file_name = 'task_' + task + '_search_line-search_matrix_L-BFGS_memory_' + str(m) + '_batch_' + str(b) + '.pkl'
-			with open(file_name, 'rb') as f:
+			file_path = results_folder + file_name
+			with open(file_path, 'rb') as f:
 				A = pickle.load(f)
 				print('max score for ',task,' = ',max(max(A[1]),max(A[4])))
+				print('time in hour = ', int(A[5]/3600.0))
 
 
 for task in tasks:
@@ -26,7 +28,8 @@ for task in tasks:
 	for m in memory_list: 
 		for b in batch_list:
 			file_name = 'task_' + task + '_search_line-search_matrix_L-BFGS_memory_' + str(m) + '_batch_' + str(b) + '.pkl'
-			with open(file_name, 'rb') as f:
+			file_path = results_folder + file_name
+			with open(file_path, 'rb') as f:
 				A = pickle.load(f)
 				max_score = max(max_score, max(max(A[1]),max(A[4])))
 	print('max score for ',task,' = ',max_score)
